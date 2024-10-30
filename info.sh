70050902af3f558ddbfd3768dcaee7849ae19000
Amin71117 <116521604+Amin71117@users.noreply.github.com>
2024-10-30 22:02:52 +0100

    Add info.sh script and other files

---
+++ info.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+# Aktuelle Systemzeit (Datum und Uhrzeit)
+echo "Aktuelle Systemzeit: $(date)"
+
+# Skriptname
+echo "Skriptname: $0"
+
+# Aktuelle Shell
+echo "Aktuelle Shell: $SHELL"
